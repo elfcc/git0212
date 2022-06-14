549fce51870c239487c1e332e223a48639c9520d
elf <elf2022206@163.com>
2022-06-14 14:39:33 +0800

    third commit

--- src/main/java/com/atguigu/GitTest.java
+++ src/main/java/com/atguigu/GitTest.java
@@ -1,13 +1,9 @@
 package com.atguigu;
 
-/**
- * @author cc
- * @create 2022/6/14 001414:29
- * @Version 1.0
- */
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("hello git1");
+        System.out.println("hello git2");
     }
 }
